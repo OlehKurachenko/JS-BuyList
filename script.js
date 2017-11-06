3ac49c37390f8f3f12a48e8d879581008fdfa451
OlehKurachenko <oleh.kurachenko@gmail.com>
2017-11-06 22:47:35 +0200

    Task 3 (9.10) finished

---
+++ script.js
@@ -0,0 +1,136 @@
+$(function () {
+    var LIST = $('.main .center .left .container');
+    var ITEM_TEMPLATE = $('.main .center .left .container').html();
+    var RIGHT_LIST = $('#rActive');
+    var RIGHT_LIST_PURCHASED = $('#rPurchased');
+    var RIGHT_ITEM_TEMPLATE = $('#rActive').html();
+
+    var CORRECT_PURCHASED_TEXT = $(ITEM_TEMPLATE).find('#purchaseButton').text();
+
+    $('.main .center .left .container').html('');
+    $('#rActive').html('');
+
+    var ANIMATION_TIME = 700;
+    var APPEAR_TIME = 150;
+
+    $('#addButton').click(function () {
+        var title = $('#input').val();
+
+        if (title)
+            addItem(title);
+    });
+
+    $('#input').keyup(function () {
+        if (event.keyCode === 13)
+        {
+            var title = $('#input').val();
+
+            if (title)
+                addItem(title);
+        }
+    });
+
+    $('#input').focus(function () {
+        $(this).select();
+    });
+
+    addItem("Помідори");
+    addItem("Печиво");
+    addItem("Сир");
+
+    function addItem(title) {
+        console.log(title);
+
+        var node = $(ITEM_TEMPLATE);
+        var right_node = $(RIGHT_ITEM_TEMPLATE);
+        var right_node_2 = $(RIGHT_ITEM_TEMPLATE);
+
+        node.find('#productName').text(title);
+        right_node.find('#rProductName').text(title);
+        right_node_2.find('#rProductName').text(title);
+
+        node.find('#removeButton').click(function () {
+            node.fadeOut(ANIMATION_TIME);
+            setTimeout(function () {
+                right_node.remove();
+                node.remove();
+            }, ANIMATION_TIME + 10);
+        });
+
+        node.find('#incButton').click(function () {
+            var count = parseInt(node.find('#quantity').text());
+            count += 1;
+            node.find('#incButton').prop("disabled",true);
+            node.find('#quantity').fadeOut(APPEAR_TIME);
+            setTimeout(function () {
+                node.find('#quantity').text(count);
+                right_node.find('#rQuantity').text(count);
+                right_node_2.find('#rQuantity').text(count);
+                node.find('#quantity').fadeIn(APPEAR_TIME);
+                node.find('#incButton').prop("disabled",false);
+                node.find('#decButton').prop("disabled",false);
+            }, APPEAR_TIME);
+        });
+
+        node.find('#decButton').prop("disabled", true);
+        node.find('#decButton').click(function () {
+            var count = parseInt(node.find('#quantity').text());
+            count -= 1;
+            node.find('#decButton').prop("disabled",true);
+            node.find('#quantity').fadeOut(APPEAR_TIME);
+            setTimeout(function () {
+                node.find('#quantity').text(count);
+                right_node.find('#rQuantity').text(count);
+                right_node_2.find('#rQuantity').text(count);
+                node.find('#quantity').fadeIn(APPEAR_TIME);
+                if (count > 1)
+                    node.find('#decButton').prop("disabled",false);
+            }, APPEAR_TIME);
+        });
+
+        node.find('#purchaseButton').click(function () {
+            if (node.find('#purchaseButton').text().localeCompare(CORRECT_PURCHASED_TEXT) == 0) {
+                node.fadeOut(APPEAR_TIME);
+                setTimeout(function () {
+                    node.find("#incButton").hide();
+                    node.find("#decButton").css('transform', 'scale(0, 0)');
+                    node.find("#removeButton").hide();
+                    node.find("#purchaseButton").css('width', '116');
+                    node.find("#purchaseButton").text("Не куплено");
+
+                    node.fadeIn();
+                    right_node.hide();
+                    RIGHT_LIST_PURCHASED.append(right_node_2);
+                }, APPEAR_TIME);
+            } else {
+                node.fadeOut(APPEAR_TIME);
+                setTimeout(function () {
+                    node.find("#incButton").show();
+                    node.find("#decButton").css('transform', 'scale(1, 1)');
+                    node.find("#removeButton").show();
+                    node.find("#purchaseButton").css('width', 'auto');
+                    node.find("#purchaseButton").text(CORRECT_PURCHASED_TEXT);
+
+                    node.fadeIn();
+                    right_node.show();
+                    right_node_2.remove();
+                }, APPEAR_TIME);
+            }
+        });
+
+        // TODO configure unPurchase button
+
+        // TODO configure right pane
+
+        node.fadeIn(ANIMATION_TIME);
+        //right_node.fadeIn("slow");
+
+        LIST.append(node);
+        setTimeout(function(){
+            RIGHT_LIST.append(right_node)
+        }, ANIMATION_TIME + 10);
+
+        $('#input').val("Назва товару");
+        $('#input').select();
+    }
+});
